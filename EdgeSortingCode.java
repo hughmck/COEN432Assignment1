05077d16d58be60ce3f1aaceab08d6f2a75c8261
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-10-07 12:27:32 -0400

    adding scanner and printer for array

--- EdgeSortingCode.java
+++ EdgeSortingCode.java
@@ -1,5 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class EdgeSortingCode {
 
+    public EdgeSortingCode() throws FileNotFoundException {
+    }
 
+    public static void mutate(){
+
+    }
+
+    public static void crossover(){
+
+    }
+
+    public static void initializingArray() throws FileNotFoundException {
+        int totalRow = 8;
+        int totalColumn = 8;
+        int[][] edgeMatchingArray = new int[totalRow][totalColumn];
+        File file = new File("Ass1Input.txt");
+        Scanner scanner = new Scanner(file);
+
+
+        for (int row = 0; scanner.hasNextLine() && row < totalRow; row++) {
+            char[] chars = scanner.nextLine().toCharArray();
+            for (int i = 0; i < totalColumn && i < chars.length; i++) {
+                edgeMatchingArray[row][i] = chars[i];
+            }
+        }
+    }
+    public static void PrintArray(int[][] edgeMatchingArray) {
+        for (int row = 0; row < edgeMatchingArray.length; row++) {
+            System.out.println(edgeMatchingArray[row]);
+        }
+    }
+
+    public static void main(String[] args)
+            throws IOException, InterruptedException {
+
+        System.out.println("Given Array from input:");
+        PrintArray(edgeMatchingArray);
 
 }
