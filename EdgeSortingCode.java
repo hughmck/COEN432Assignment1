87f9d9e367d6026d976df70f8d374e9439215621
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-10-21 15:00:00 -0400

    adding rotate function

--- EdgeSortingCode.java
+++ EdgeSortingCode.java
@@ -173,10 +173,33 @@ public class EdgeSortingCode {
 
     public static void swapRowCrossover(int[][] populationMatrix, int A, int B){ //taking row A and row B in matrix, swapping them
 
-        int[] temp = populationMatrix[A - 1];
-        populationMatrix[A - 1]= populationMatrix[B - 1];
-        populationMatrix[B - 1] = temp;
+        int[] temp = populationMatrix[A];
+        populationMatrix[A]= populationMatrix[B];
+        populationMatrix[B] = temp;
+    }
 
+    static int rotateMutation(int [][] array, int i0, int j0){
+
+        Random random = new Random();
+        i0 = random.nextInt(7);
+
+        Random random1 = new Random();
+        j0 = random.nextInt(7);
+
+        int arrayValue = 0;
+
+        arrayValue = array[i0][j0];
+
+        int result = 0;
+        int place = 1;
+
+        result += place * 10 * (arrayValue % 10);
+        arrayValue /= 10;
+        result += place * (arrayValue % 10);
+        arrayValue /= 10;
+        place *= 100;
+
+        return result + place * arrayValue;
     }
 
 
@@ -188,12 +211,12 @@ public class EdgeSortingCode {
             int rows = 8;
             int columns = 8;
             
-            int [][] arrayRepresentation = new int[rows][columns];
+            int [][] selectionArray = new int[rows][columns];
             while(sc.hasNextLine()) {
-                for (int i=0; i<arrayRepresentation.length; i++) {
+                for (int i=0; i<selectionArray.length; i++) {
                     String[] line = sc.nextLine().trim().split(" ");
                     for (int j=0; j<line.length; j++) {
-                        arrayRepresentation[i][j] = Integer.parseInt(line[j]);
+                        selectionArray[i][j] = Integer.parseInt(line[j]);
                     }
                 }
             }
@@ -202,58 +225,70 @@ public class EdgeSortingCode {
             //step 2 : random_wheel selection 
             System.out.print("hugh mckenzie 40088023 yves chuenmogne 40129773\n"); 
             System.out.print("Population\n"); 
-            int selectionArray[][] = {
-        		
+//            int selectionArray[][] = {
+//
+            //3421 3127 7632 2231 5531 6417 3746 2625
+            //1275 6135 1631 2243 3611 2337 2255 4655
+            //2123 3136 5612 3664 7342 3756 2115 5162
+            //4733 5167 7414 4256 6233 5513 5236 7433
+            //2263 3415 2367 3145 2716 4547 2353 5225
+            //5725 3555 7422 2231 2133 2137 4614 2456
+            //3475 7562 5347 3215 5265 4563 3451 5424
+            //7222 7257 2523 4113 3762 5232 5231 2567
+//            };
 
-                {2310 ,2016, 6521, 1120, 4420, 5306, 2635, 1514},
-                {1164 ,5024 ,1520, 1132, 2500, 1226, 1144, 3544},
-                {1012 ,2025 ,4501, 2553 ,6231, 2645, 1004, 4051},
-                {3622 ,4056, 6303, 3145, 5122, 4403, 4125, 6322},
-                {1152 ,2304 ,1256, 2034 ,1605, 3436, 1242, 4114},
-                {4614 ,2444, 6311 ,1120, 1022, 1026, 3503, 1345},
-                {2364 ,6451, 4236 ,2104, 4514, 3452, 2340, 4313},
-                {6111 ,6146, 1412 ,3002 ,2651, 4121, 4120 ,1456}
-            };
-           // printBestSolution(selectionArray);
-
-            int upperbound = 8;
+            int upperbound = 7;
 
             Random random1 = new Random();
-            int x = random1.nextInt(upperbound) - 1 ; //trying to find a number between 1 and 8 to swap
+            int x = random1.nextInt(upperbound); //trying to find a number between 1 and 8 to swap
+
+        System.out.println(x);
 
             Random random2 = new Random();
-            int y = random2.nextInt(upperbound) - 1 ; //trying to find a number between 1 and 8 to find the column to swap
+            int y = random2.nextInt(upperbound); //trying to find a number between 1 and 8 to find the column to swap
 
-            Random random3 = new Random();
-            int a = random3.nextInt(upperbound) - 1; //next 4 integers are random numbers between 0 and 8 for the index number of two numbers in the array to perform a swap on
+        System.out.println(y);
 
-            Random random4 = new Random();
-            int b = random4.nextInt(upperbound) - 1;
 
-            Random random5 = new Random();
-            int c = random5.nextInt(upperbound) - 1;
+        Random random3 = new Random();
+            int a = random3.nextInt(upperbound); //next 4 integers are random numbers between 0 and 8 for the index number of two numbers in the array to perform a swap on
 
-            Random random6 = new Random();
-            int d = random6.nextInt(upperbound) - 1;
+        System.out.println(a);
 
-            int test = computeFitness(selectionArray, 8, 8); //computes the number of mismatched edges in the entire array
+        Random random4 = new Random();
+            int b = random4.nextInt(upperbound);
+
+        System.out.println(b);
+
+
+        Random random5 = new Random();
+            int c = random5.nextInt(upperbound);
+
+        System.out.println(c);
+
+
+        Random random6 = new Random();
+            int d = random6.nextInt(upperbound);
+
+        System.out.println(d);
+
+        System.out.println();
+
+        int test = computeFitness(selectionArray, 8, 8); //computes the number of mismatched edges in the entire array
             // log("Mismatches found: " + test + "\n");
 
 
             int iterationCounter = 0;
 
 
-            while (test <= 50){
+            while (test >= 50 && iterationCounter < 100000){
 
                 swapRowCrossover(selectionArray, x, y); //swaps rows at random
                 randomElementCrossover(selectionArray, a, b, c, d); //swaps two random numbers in the array
+                test = computeFitness(selectionArray,8,8);
+                rotateMutation(selectionArray, x, y);
                 iterationCounter++;
-
             }
-
-            if (test <= 50 || iterationCounter > 100000){
-                printBestSolution(selectionArray);
-            }
+        printBestSolution(selectionArray);
     }
-
 }
